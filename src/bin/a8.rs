6a9c7c3930a49ce71c06d8927fefb7b321c09f79
Raymond <73725736+raymondanythings@users.noreply.github.com>
2022-11-09 00:01:10 +0900

    tuple

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,37 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavor {
+    Sparkling,
+    Sweet,
+    Fruity,
+}
+
+struct Drink {
+    flavor: Flavor,
+    fluid_oz: f64,
+}
+
+fn print_drint(drink: Drink) {
+    match drink.flavor {
+        Flavor::Sparkling => println!("flavor : Sparkling"),
+        Flavor::Sweet => println!("flavor : Sweet"),
+        Flavor::Fruity => println!("flavor : Fruity"),
+    }
+
+    println!("oz: {:?}", drink.fluid_oz);
+}
+
+fn main() {
+    let sweet = Drink {
+        flavor: Flavor::Sweet,
+        fluid_oz: 6.2,
+    };
+    print_drint(sweet);
+
+    let fruity = Drink {
+        flavor: Flavor::Fruity,
+        fluid_oz: 6.2,
+    };
+    print_drint(fruity);
+}
