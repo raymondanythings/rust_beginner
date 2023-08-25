c64a8317f0c2931d632fd78db0ca9af45f3b2cbd
Raymond <akdfhr2@gmail.com>
2023-08-25 15:11:35 +0900

    reset repo

--- hello/target/debug/deps/hello.d
+++
@@ -1,5 +0,0 @@
-C:\Users\KitWorks\Documents\rust-programming-for-beginners\hello\target\debug\deps\hello.exe: src\main.rs
-
-C:\Users\KitWorks\Documents\rust-programming-for-beginners\hello\target\debug\deps\hello.d: src\main.rs
-
-src\main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-C:\Users\KitWorks\Documents\rust-programming-for-beginners\hello\target\debug\hello.exe: C:\Users\KitWorks\Documents\rust-programming-for-beginners\hello\src\main.rs
